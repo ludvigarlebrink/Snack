aa6b2c8977ff5e2751abeefc525d91ea62f33b88
ludvigarlebrink <ludvig.arlebrink@outlook.com>
2019-03-29 15:28:48 +0100

    shade raid

---
+++ Source/ShadeRaid/Public/Nodes/GeometryEntryNode.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "BaseNode.hpp"
+
+namespace spy
+{
+class SHADE_RAID_API GeometryEntryNode final : public BaseNode
+{
+public:
+
+    GeometryEntryNode();
+
+    ~GeometryEntryNode();
+
+private:
+
+};
+}
