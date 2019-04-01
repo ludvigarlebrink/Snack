23c8973aa3a0957c7200545f7e568b3e55ff8925
AsharImtiaz <ashar.imtiaz1992@gmail.com>
2019-04-01 10:17:20 +0200

    Update MaterialWindow.cpp

--- Source/Editor/Private/EditorWindows/MaterialWindow.cpp
+++ Source/Editor/Private/EditorWindows/MaterialWindow.cpp
@@ -1,50 +1,49 @@
-#include"EditorWindows/MaterialWindow.hpp"
+#include "EditorWindows/MaterialWindow.hpp"
 #include "EngineInclude.hpp"
 #include "RenderCoreInclude.hpp"
 #include "SketchInclude.hpp"
 
 namespace spy
 {
-    MaterialWindow::MaterialWindow()
-        : m_material(nullptr)
-    {
-        SetUp();
-    }
-
-    MaterialWindow::~MaterialWindow()
-    {
-        TearDown();
-    }
-
-    std::string MaterialWindow::GetTitle()
-    {
-        return "Material";
-    }
-
-    void MaterialWindow::OnDraw(f32 deltaTime)
-    {
-        glm::vec4 color(0.00,0.00,0.00,0.00);
-        glm::vec4 texture(0.00, 0.00, 0.00, 0.00);
-        Sketch::Vec4Field("Color",color);
-        if (m_material)
-        {
-            m_material->SetColor("Test", color);
-        }
-        //test.SetColor
-        Sketch::Vec4Field("Texture",texture);
-    }
-
-    void MaterialWindow::SetUp()
-    {
-      
-    }
-
-    void MaterialWindow::TearDown()
-    {
-        //delete m_texture;
-        //m_texture = nullptr;
-        //delete m_framebuffer;
-        //m_framebuffer = nullptr;
-    }
+MaterialWindow::MaterialWindow()
+    : m_material(nullptr)
+{
+    SetUp();
 }
 
+MaterialWindow::~MaterialWindow()
+{
+    TearDown();
+}
+
+std::string MaterialWindow::GetTitle()
+{
+    return "Material";
+}
+
+void MaterialWindow::OnDraw(f32 deltaTime)
+{
+    glm::vec4 color(0.00,0.00,0.00,0.00);
+    glm::vec4 texture(0.00, 0.00, 0.00, 0.00);
+    Sketch::Vec4Field("Color",color);
+    if (m_material)
+    {
+        m_material->SetColor("Test", color);
+    }
+    //test.SetColor
+    Sketch::Vec4Field("Texture",texture);
+}
+
+void MaterialWindow::SetUp()
+{
+  
+}
+
+void MaterialWindow::TearDown()
+{
+    //delete m_texture;
+    //m_texture = nullptr;
+    //delete m_framebuffer;
+    //m_framebuffer = nullptr;
+}
+} // namespace spy
