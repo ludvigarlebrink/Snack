0f0370675399564753a41a1c62aae24283cc7363
ludvigarlebrink <ludvig.arlebrink@outlook.com>
2019-04-09 19:41:32 +0200

    terrain editor window

---
+++ Source/Editor/Private/EditorWindows/TerrainEditorWindow.cpp
@@ -0,0 +1,12 @@
+#include "EditorWindows\TerrainEditorWindow.hpp"
+
+namespace spy
+{
+TerrainEditorWindow::TerrainEditorWindow()
+{
+}
+
+TerrainEditorWindow::~TerrainEditorWindow()
+{
+}
+} // namespace spy
---
+++ Source/Editor/Public/EditorWindows/TerrainEditorWindow.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "EditorWindow.hpp"
+
+namespace spy
+{
+class EDITOR_API TerrainEditorWindow : public EditorWindow
+{
+public:
+
+    TerrainEditorWindow();
+
+    ~TerrainEditorWindow();
+
+private:
+
+
+};
+} // namespace spy
