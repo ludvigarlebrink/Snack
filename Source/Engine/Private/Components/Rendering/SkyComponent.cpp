a831f50476a7ba7e99e4deb741635d28f173bc2a
ludvigarlebrink <ludvig.arlebrink@outlook.com>
2019-04-11 11:51:21 +0200

    sky component

---
+++ Source/Engine/Private/Components/Rendering/SkyComponent.cpp
@@ -0,0 +1,13 @@
+#include "Components/Rendering/SkyComponent.hpp"
+
+namespace snack
+{
+SkyComponent::SkyComponent(Transform* transform)
+    : BaseComponent(transform)
+{
+}
+
+SkyComponent::~SkyComponent()
+{
+}
+}
