02af12c438b281a0613f47717cb8510ebece0ab9
ludvigarlebrink <ludvig.arlebrink@outlook.com>
2019-04-11 10:09:15 +0200

    tesselation

--- Source/Main/Data/Shaders/Terrain.vs.glsl
+++ Source/Main/Data/Shaders/Terrain.vs.glsl
@@ -5,8 +5,11 @@ layout (location = 0) in vec2 Position_VS_in;
 uniform mat4 Model = mat4(1.0);
 uniform mat4 ViewProjection = mat4(1.0);
 
+out vec3 WorldPos_TCS_in;
+
 void main()
 {
     vec4 worldPosition = Model * vec4(Position_VS_in.x, 0.0f, Position_VS_in.y, 1.0);
-    gl_Position = ViewProjection * worldPosition;
+    WorldPos_TCS_in = worldPosition.xyz;
+    gl_Position = worldPosition;
 }
